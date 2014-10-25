86212eb7f963517a2e5a3373fd3b8977a3f11721
Conor Sloan <c.sloan7597@googlemail.com>
2014-10-25 19:15:28 +0100

    First test class

---
+++ test/main/java/javapad/client/controllers/JavaPadControllerTest.java
@@ -0,0 +1,23 @@
+package javapad.client.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by conor on 25/10/2014.
+ */
+public class JavaPadControllerTest {
+
+    JavaPadController controller;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void someTest() {
+        System.out.println("Hello World");
+    }
+
+}
